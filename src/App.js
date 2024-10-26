0b5ab84987ab6167196598c8d4f173c5438bc5d6
mmustafa56 <muhammadmustafapak71@gmail.com>
2024-10-26 17:46:32 +0400

    modified the app.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <h3>this is after the setup of ci/cd pipeline.</h3>
+    <h3>this is after the setup of ci/cd pipeline.</h3>
+    <h3>this is after the setup of ci/cd pipeline.</h3>
     <h3>This is the second deployment to the github pages.</h3>
     <h3>This is the second deployment to the github pages.</h3>
     <h3>This is the second deployment to the github pages.</h3>
     <h3>This is the second deployment to the github pages.</h3>
+    <h3>this is after the setup of ci/cd pipeline.</h3>
     </div>
   );
 }
